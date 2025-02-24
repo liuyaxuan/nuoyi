b18832c8f6a29303f517d79ec71bfb1ff2282b5c
Liu.Y.X <651409800@qq.com>
2025-02-24 15:28:20 +0800

    左侧导航菜单

--- src/components/menu/index.vue
+++ src/components/menu/index.vue
@@ -1,52 +1,39 @@
 <template>
     <div class="menu-container">
-        <el-menu
-            :default-active="defaultActive"
-            class="el-menu-vertical-demo"
-            :collapse="isCollapse"
-        >
-            <el-sub-menu
-                v-for="(item) in refRac.menuData"
-                :key="item.path"
-                :index="item.path"
+        <div class="menu-item" v-for="(item, index) in refRac.menuData" :key="item.path">
+            <div
+                v-if="!item.hidden"
+                :class="`ny-menu menu-title parent-${index}`"
+                @click="handleMenuToggle(item, index)"
             >
-                    <template #title v-if="!item.hidden">
-                        <el-icon><house /></el-icon>
-                        <span>{{ item.meta.title || '' }}</span>
-                    </template>
-                    <template
-                        v-for="(childrenItem) in item.children"
-                        :key="childrenItem.path"
-                    >
-                        <el-menu-item
-                            v-if="item.children && item.children.length > 0 && !childrenItem.hidden"
-                            :index="childrenItem.path"
-                        >
-                            <template #title>
-                                <router-link :to="item.path + '/' + childrenItem.path">
-									<el-icon><Menu /></el-icon>
-									{{ childrenItem.meta.title || '' }}
-								</router-link>
-                            </template>
-                        </el-menu-item>
-                    </template>
-            </el-sub-menu>
-        </el-menu>
-		<div class="collapse-btn" @click="isCollapse = !isCollapse">
-			<el-icon>
-				<Expand v-if="isCollapse" />
-				<Fold v-else />
-			</el-icon>
-		</div>
-		
+                <el-icon>
+                    <house />
+                </el-icon>
+                <span>{{ item.meta.title || '' }}</span>
+            </div>
+            <div :class="`ny-menu menu-children children-${index}`">
+                <div
+                    class="ny-menu menu-children-item"
+                    v-for="(childrenItem, index2) in item.children"
+                    :key="childrenItem.path"
+                    v-show="!childrenItem.hidden"
+                    @click.stop="handleMenuSelect($event)"
+                >
+                    <router-link
+                        :to="item.path + '/' + childrenItem.path">
+                        {{ childrenItem.meta.title || '' }}
+                    </router-link>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import {
     ref,
-	watch,
-	computed,
+    watch,
+    computed,
     reactive,
     onMounted,
     getCurrentInstance,
@@ -57,40 +44,66 @@ import { useRouter, useRoute } from 'vue-router'
 export default {
     name: "my-menu",
     props: [],
-    setup(props,context) {
-		const store = useStore();
-		const $router = useRouter(); // 获取路由实例
-		const $route = useRoute(); // 获取当前路由信息
+    setup(props, context) {
+        const store = useStore();
+        const $router = useRouter(); // 获取路由实例
+        const $route = useRoute(); // 获取当前路由信息
         // 获取当前组件的实例
         const app = getCurrentInstance();
         // 是否折叠
         const isCollapse = ref(false);
         // 默认选中菜单
         const defaultActive = ref('');
+        const menuHeight = ref(0);
 
         const refRac = reactive({
             menuData: []
         })
-		
-		// 监听
-		watch(isCollapse, (newVal, oldVal) => {
-			context.emit('sentState', newVal);
-		})
-		watch(() => $route, (newVal, oldVal) => {
-			const { fullPath } = newVal;
-			setTimeout(() => {
-				defaultActive.value = $route.name;
-			}, 0)
-		}, { immediate: true, deep: true })
 
-        // 展开
-        const handleOpen = (key, keyPath) => {
-        // console.log(key, keyPath)
+        // 监听
+        watch(isCollapse, (newVal, oldVal) => {
+            context.emit('sentState', newVal);
+        })
+        watch(() => $route, (newVal, oldVal) => {
+            const { fullPath } = newVal;
+            setTimeout(() => {
+                defaultActive.value = $route.name;
+            }, 0)
+        }, { immediate: true, deep: true })
+
+        // 展开、收起
+        const handleMenuToggle = (node, key) => {
+            // 展开、收起动作
+            const content = document.getElementsByClassName('children-' + key)[0];
+            if (content.offsetHeight > 0) {
+                menuHeight.value = content.offsetHeight;
+                const timer = setInterval(() => {
+                    const h = content.offsetHeight - 1;
+                    content.style.height = h + 'px';
+                    if (h == 0) {
+                        clearInterval(timer);
+                    }
+                }, 5)
+            } else {
+                const timer = setInterval(() => {
+                    let h = content.offsetHeight + 1;
+                    content.style.height = h + 'px';
+                    if (h == menuHeight.value) {
+                        clearInterval(timer);
+                        menuHeight.value = h;
+                    }
+                }, 5) 
+            }
         }
 
-        // 收起
-        const handleClose = (key, keyPath) => {
-        // console.log(key, keyPath)
+        // 选中菜单
+        const handleMenuSelect = (e) => {
+            // 样式控制
+            const menu = document.getElementsByClassName('ny-menu');
+            for (let i = 0; i < menu.length; i++) {
+                menu[i].classList.remove('ny-menu-active');
+            }
+            e.target.classList.add('ny-menu-active');
         }
 
         // 获取生成菜单树结构
@@ -129,11 +142,12 @@ export default {
             refRac,
             isCollapse,
             defaultActive,
+            menuHeight,
             // 函数
-            handleOpen,
-            handleClose,
+            handleMenuToggle,
             createdMenuTree,
             handleDefaultActive,
+            handleMenuSelect,
         }
     },
 }
@@ -145,23 +159,62 @@ export default {
     height: 100%;
     display: flex;
     flex-direction: column;
-    
+
     a {
         width: 100%;
         color: #fff;
         display: block;
         text-align: left;
     }
-	
-	.collapse-btn {
-		width: 100%;
-		height: 40px;
-		line-height: 40px;
-		text-align: center;
-		cursor: pointer;
-		background-color: #3f9eff;
-		color: #fff;
-		font-size: 16px;
-	}
+
+    .collapse-btn {
+        width: 100%;
+        height: 40px;
+        line-height: 40px;
+        text-align: center;
+        cursor: pointer;
+        background-color: #3f9eff;
+        color: #fff;
+        font-size: 16px;
+    }
+}
+.menu-item {
+    width: 100%;
+    height: auto;
+    text-align: left;
+    font-size: 14px;
+    color: #fff;
+    overflow-y: hidden;
+}
+
+
+.hidden {
+    max-height: 0;
+    overflow: hidden;
+    transition: max-height 0.5s ease-out;
+}
+.expanded {
+    transition: max-height 0.5s ease-in;
+}
+
+.menu-title {
+    box-sizing: border-box;
+    padding: 10px 10px;
+}
+.menu-children-item, .menu-title {
+    cursor: pointer;
+    transition: all 0.3s;
+
+    &:hover {
+        background-color: #3f9eff;
+    }
+}
+.menu-children-item a {
+    box-sizing: border-box;
+    padding: 10px 0px 10px 40px;
+}
+
+.ny-menu-active {
+    background-color: #3f9eff;
 }
 </style>
